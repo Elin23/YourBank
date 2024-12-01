d85a2041faed4de750bc03468362b91c9e61eb8b
Elin23 <elinshaia23@gmail.com>
2024-12-02 00:16:19 +0300

    adding FAQs & Use Cases

---
+++ src/Pages/Careers/Careers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FAQSectionComponent from '../../Components/FAQSectionComponent/FAQSectionComponent'
+
+export default function Careers() {
+  return (
+    <div>
+
+    </div>
+  )
+}
