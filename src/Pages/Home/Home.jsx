2362e6fe3c7a0c6e1804fbb974347860b5c7070f
DELL <you@example.com>
2024-12-03 12:08:56 +0200

    features section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import FeaturesSectionComponent from '../../Components/FeaturesSectionComponent/FeaturesSectionComponent'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <FeaturesSectionComponent />
+    </>
   )
 }
