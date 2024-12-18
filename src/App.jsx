ec08967d8518bd201bffc49fb93147d996674dfd
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2024-12-19 01:11:56 +0300

    add productsec-productcard-titlecomp

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-// import { Routes, Route } from 'react-router-dom';
-// import About from './Pages/About/About'
-// import Careers from './Pages/Careers/Careers'
-// import Home from './Pages/Home/Home'
-// import Login from './Pages/Login/Login'
-// import Security from './Pages/Security/Security'
-// import SignUp from './Pages/SignUp/SignUp'
+import { Routes, Route } from 'react-router-dom';
+import About from './Pages/About/About'
+import Careers from './Pages/Careers/Careers'
+import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
+import Security from './Pages/Security/Security'
+import SignUp from './Pages/SignUp/SignUp'
 import "./App.css";
-//  
-import ProductSectionComponent from "./Components/ProductSectionComponent/ProductSectionComponent";
-// import TitleComponent from "./Components/TitleComponent/TitleComponent";
 
 function App() {
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/careers' element={<Careers />} />
         <Route path='/security' element={<Security />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signUp' element={<SignUp />} />
-      </Routes> */}
-      {/* <TitleComponent
-        titleLeft=""
-        desc="Discover a range of comprehensive and customizable banking products at YourBank, designed to suit your unique financial needs and aspirations"
-        span="ccccc"
-        titleRight="aaaaa"
-      /> */}
-      <ProductSectionComponent />
-      {/* <ProductCardComponent/> */}
+      </Routes>
     </>
   );
 }
