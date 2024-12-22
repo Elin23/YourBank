f1b2dbb1ad35f5da25ceb49148ddae10134548b3
DELL <you@example.com>
2024-12-22 03:28:39 +0200

    test

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PressReleasesSectionComponent from '../../Components/PressReleasesSectionComponent/PressReleasesSectionComponent'
 
 export default function About() {
   return (
     <div>
-      
+      <PressReleasesSectionComponent />
     </div>
   )
 }
