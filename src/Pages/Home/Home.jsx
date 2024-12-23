015c5b10077313b6205fb09087fe975cb0c6b89a
hasanWhatfa <wtfyh05@gmail.com>
2024-12-23 22:37:15 +0300

    adding the marginY for the Adsection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import UseCasesSectionComponent from './../../Components/UseCasesSectionComponen
 import FAQSectionComponent from './../../Components/FAQSectionComponent/FAQSectionComponent'
 import AdSectionComponent from './../../Components/AdSectionComponent/AdSectionComponent'
 import AdSecData from './../../Data/AdSectionData'
+import FooterComponent from '../../Components/FooterComponent/FooterComponent'
 export default function Home() {
   return (
     <div>
       <UseCasesSectionComponent />
       <FAQSectionComponent />
       <AdSectionComponent DATA={AdSecData[0]} />
+      <FooterComponent />
     </div>
   )
 }
