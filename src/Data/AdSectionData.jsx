307a8b2bb42704d1e2576aaeaa3a90eff9662212
hasanWhatfa <wtfyh05@gmail.com>
2024-12-30 02:43:38 +0300

    small edits

--- src/Data/AdSectionData.jsx
+++ src/Data/AdSectionData.jsx
@@ -1,16 +1,3 @@
-// const AdSecData =
-// [
-//     {
-//         title:'Start your financial journey with ',
-//         desc:'Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus.'
-//     },
-//     {
-//         title:'Start your Career ',
-//         desc:'Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus.'
-//     }
-// ]
-// export default AdSecData;
-
 const storedValueAdASec = localStorage.getItem('AdSecData');
 
 const AdSecData = storedValueAdASec ? JSON.parse(storedValueAdASec) : [
