a17c1fd13ec461659910fd637218a0bced59bb66
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2025-01-14 17:16:50 +0300

    commit

--- src/Components/HandleLoadingComponent/HandleLoadingComponent.jsx
+++ src/Components/HandleLoadingComponent/HandleLoadingComponent.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import LoaderComponent from "../LoaderComponent/LoaderComponent";
 import { Outlet } from "react-router-dom";
-import AOS from "aos";
 export default function HandleLoadingComponent() {
   const [loading, setLoading] = useState(true);
 
