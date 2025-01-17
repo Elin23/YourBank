a79e48ad6c7629bed57cbeaebbaeb37e2cd182fa
Elin23 <elinshaia23@gmail.com>
2025-01-17 18:11:50 +0300

    Final Version of YourBank Project

--- src/Pages/Security/Security.jsx
+++ src/Pages/Security/Security.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import JobComponent from "../../Components/JobComponent/JobComponent";
-
 import HeroComponent2 from '../../Components/HeroComponent2/HeroComponent2'
 import IMG3 from '../../assets/imgs/Hero/HeroImage3.webp'
 import HowWeProtectComponent from '../../Components/HowWeProtectComponent/HowWeProtectComponent';
+import FAQSectionComponent from '../../Components/FAQSectionComponent/FAQSectionComponent';
 export default function Security() {
   return (
     <>
@@ -12,9 +11,10 @@ export default function Security() {
         image={IMG3}
         head={"Your Security is Our Top Priority"}
         light={"Top Priority"}
-        disc={"At YourBank, we understand the importance of keeping your financial information secure. We employ robust security measures and advanced technologies to protect your personal and financial data. Rest assured that when you bank with us, your security is our utmost priority."} />
-        <HowWeProtectComponent />
-      <JobComponent />
+        disc={"At YourBank, we understand the importance of keeping your financial information secure. We employ robust security measures and advanced technologies to protect your personal and financial data. Rest assured that when you bank with us, your security is our utmost priority."} 
+      />
+      <HowWeProtectComponent />
+      <FAQSectionComponent />
     </>
   )
 }
